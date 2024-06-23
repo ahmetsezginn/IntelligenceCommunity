a6f44ab4ba23349834d60252900f5b9fd06cb633
ahmet23272327 <ahmet.sezgin@std.hku.edu.tr>
2024-06-23 15:49:03 +0300

    new one

--- src/controllers/productController.js
+++
@@ -1,7 +0,0 @@
-exports.getAllProducts = (req, res) => {
-    res.send('Get all products');
-};
-
-exports.createProduct = (req, res) => {
-    res.send('Create a new product');
-};
--- src/controllers/userController.js
+++
@@ -1,7 +0,0 @@
-exports.getAllUsers = (req, res) => {
-    res.send('Get all users');
-};
-
-exports.createUser = (req, res) => {
-    res.send('Create a new user');
-};
